a0a11b48b73d6950a402e9c67fe0a5bc1f808b26
Sergeikami <127503226+Sergeikami@users.noreply.github.com>
2023-03-13 14:22:04 +0100

    Afegit gravetat i salt
    El cos li afecta la gravetat i salta mentre estigui al terra

--- ds/KinematicBody2D.gd
+++ ds/KinematicBody2D.gd
@@ -3,8 +3,8 @@ extends KinematicBody2D
 var moviment := Vector2.RIGHT 
 var velocitat := 15000
 var direccio := Vector2.RIGHT
-var salt := Vector2.UP
-var gravetat := Vector2.DOWN
+var salt := Vector2.UP * 500
+var gravetat := Vector2.DOWN * 1000
 
 
 
@@ -17,6 +17,8 @@ func _ready():
 func _process(delta):
 	moviment.x = velocitat * delta
 	moviment += gravetat * delta
-	moviment = move_and_slide(moviment, Vector2.UP)
+
 	if Input.is_action_just_pressed('espai') and is_on_floor():
 		moviment += salt
+	
+	moviment = move_and_slide(moviment, Vector2.UP)
